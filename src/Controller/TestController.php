263dc819c09aaaa084575142842fe195dd74121e
Med Amine <med.amine.mzk@gmail.com>
2024-02-17 20:06:43 +0100

    Symfony TP1

---
+++ src/Controller/TestController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class TestController extends AbstractController
+{
+    //attributes...........
+
+    //methods.............
+    #[Route('/hello')]
+    public function index(): Response
+    {
+        return new Response("Hello World !");
+    }
+
+    #[Route('/hello/{myName}')]
+    public function indexName($myName): Response
+    {
+        return new Response("Hello World ! ".$myName);
+    }
+
+    #[Route('/calc/add/{x}/{y}', name: 'urlCalcAdd')]
+    public function calcAdd($x,$y): Response
+    {   
+        $res = $x + $y;
+        return new Response("Calcule -ADD- ! ".$res);
+    }
+    #[Route('/calc/mul/{x}/{y}', name: 'urlCalcMul')]
+    public function calcMul($x,$y): Response
+    {   
+        $res = $x * $y;
+        return new Response("Calcule -MUL> ! ".$res);
+    }
+
+    #[Route('/showList/{genre}', name: 'showList')]
+    public function showListFilms($genre): Response
+    {   //base de donnée
+        //list des films
+        //env listFilms twig !
+        $res = $x * $y;
+        return new Response("Calcule -MUL> ! ".$res);
+    }
+
+   
+}
